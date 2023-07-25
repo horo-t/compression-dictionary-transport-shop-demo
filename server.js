b5566ffb6088d19c02cb20ed84923bb05012e8bf
Tsuyoshi Horo <horo@chromium.org>
2023-07-25 04:46:11 +0000

    Add shop demo files

---
+++ server.js
@@ -0,0 +1,82 @@
+
+const crypto = require('crypto');
+const fastify = require('fastify')({
+  logger: false,
+});
+const path = require('path');
+const fs = require('fs').promises;
+
+fastify.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/', // optional: default '/'
+});
+
+async function loadDictionary() {
+  const data = await fs.readFile(`data/shop.dict`);
+  const hash = crypto.createHash('sha256').update(data).digest('hex');
+  const compressed = await fs.readFile(`data/shop.dict.br`);
+  return { hash: hash, compressed: compressed };
+}
+
+async function loadItemData(id) {
+  const br = await fs.readFile(`data/${id}.html.br`);
+  const sbr = await fs.readFile(`data/${id}.html.sbr`);
+  return { id: id, br: br, sbr: sbr };
+}
+
+
+const ITEMS = ['1f3bf', '1f45e', '1f45f', '1f460', '1f461', '1f462', '1f6fc', '1f97e', '1f97f', '1fa70', '1fa74', '26f8'];
+
+async function loadItems() {
+  let promises = [];
+  ITEMS.forEach((id) => {
+    promises.push(loadItemData(id));
+  });
+  const itmes = await Promise.all(promises);
+  let itmesMap = {};
+  itmes.forEach((item) => {
+    itmesMap[item.id] = item;
+  })
+  return itmesMap;
+}
+
+const dictionaryPromise = loadDictionary();
+const itemsPromise = loadItems();
+
+fastify.get(`/dictionary`, async function (request, reply) {
+  const dictionary = await dictionaryPromise;
+  reply.header('cache-control', 'public, max-age=100');
+  reply.header('use-as-dictionary', 'match="/items/*"');
+  reply.header('content-encoding', 'br');
+  reply.send(Buffer.from(dictionary.compressed));
+});
+
+ITEMS.forEach((id) => {
+  fastify.get(`/items/${id}.html`, async function (request, reply) {
+    const dictionary = await dictionaryPromise;
+    const items = await itemsPromise;
+    reply.header('content-type', 'text/html; charset=utf-8');
+    reply.header('cache-control', 'public, max-age=100');
+    reply.header('vary', 'sec-available-dictionary');
+    const dictHash = request.headers['sec-available-dictionary'];
+    if (dictHash == dictionary.hash) {
+      reply.header('content-encoding', 'sbr');
+      reply.send(Buffer.from(items[id].sbr));
+    } else {
+      reply.header('content-encoding', 'br');
+      reply.send(Buffer.from(items[id].br));
+    }
+  });
+});
+
+fastify.listen(
+  { port: process.env.PORT, host: '0.0.0.0' },
+  function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+    fastify.log.info(`server listening on ${address}`);
+  }
+);
---
+++ setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -e
+
+readonly BROTLI_REV="779a49bfd67793f64b43015a85bc5965fba2063f"
+
+rm -rf third_party
+
+mkdir third_party
+cd third_party
+
+# Download brotli
+
+git clone https://github.com/google/brotli.git
+cd brotli
+git checkout $BROTLI_REV
+
+bazel build brotli
+
+cd research/
+
+bazel build dictionary_generator
+
+cd ../../../
+
+cd data
+
+rm -f *.br *.sbr *.dict
+
+../third_party/brotli/research/bazel-bin/dictionary_generator -t120k ./shop.dict ./*.html
+
+../third_party/brotli/bazel-bin/brotli 1f3bf.html -o 1f3bf.html.br
+../third_party/brotli/bazel-bin/brotli 1f45e.html -o 1f45e.html.br
+../third_party/brotli/bazel-bin/brotli 1f45f.html -o 1f45f.html.br
+../third_party/brotli/bazel-bin/brotli 1f460.html -o 1f460.html.br
+../third_party/brotli/bazel-bin/brotli 1f461.html -o 1f461.html.br
+../third_party/brotli/bazel-bin/brotli 1f462.html -o 1f462.html.br
+../third_party/brotli/bazel-bin/brotli 1f6fc.html -o 1f6fc.html.br
+../third_party/brotli/bazel-bin/brotli 1f97e.html -o 1f97e.html.br
+../third_party/brotli/bazel-bin/brotli 1f97f.html -o 1f97f.html.br
+../third_party/brotli/bazel-bin/brotli 1fa70.html -o 1fa70.html.br
+../third_party/brotli/bazel-bin/brotli 1fa74.html -o 1fa74.html.br
+../third_party/brotli/bazel-bin/brotli 26f8.html  -o  26f8.html.br
+
+../third_party/brotli/bazel-bin/brotli shop.dict -o shop.dict.br
+
+../third_party/brotli/bazel-bin/brotli 1f3bf.html -D shop.dict -o 1f3bf.html.sbr
+../third_party/brotli/bazel-bin/brotli 1f45e.html -D shop.dict -o 1f45e.html.sbr
+../third_party/brotli/bazel-bin/brotli 1f45f.html -D shop.dict -o 1f45f.html.sbr
+../third_party/brotli/bazel-bin/brotli 1f460.html -D shop.dict -o 1f460.html.sbr
+../third_party/brotli/bazel-bin/brotli 1f461.html -D shop.dict -o 1f461.html.sbr
+../third_party/brotli/bazel-bin/brotli 1f462.html -D shop.dict -o 1f462.html.sbr
+../third_party/brotli/bazel-bin/brotli 1f6fc.html -D shop.dict -o 1f6fc.html.sbr
+../third_party/brotli/bazel-bin/brotli 1f97e.html -D shop.dict -o 1f97e.html.sbr
+../third_party/brotli/bazel-bin/brotli 1f97f.html -D shop.dict -o 1f97f.html.sbr
+../third_party/brotli/bazel-bin/brotli 1fa70.html -D shop.dict -o 1fa70.html.sbr
+../third_party/brotli/bazel-bin/brotli 1fa74.html -D shop.dict -o 1fa74.html.sbr
+../third_party/brotli/bazel-bin/brotli 26f8.html  -D shop.dict -o  26f8.html.sbr
+
+ls -al
+
+npm install
+
+npm start run
