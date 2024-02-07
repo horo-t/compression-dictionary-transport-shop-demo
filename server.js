51d14b3dc6c68970663f72975a36245a842de80e
Tsuyoshi Horo <horo@chromium.org>
2024-02-07 08:31:56 +0000

    Support V2 backend

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ fastify.register(require('@fastify/static'), {
 
 async function loadDictionary() {
   const data = await fs.readFile(`data/shop.dict`);
-  const hash = crypto.createHash('sha256').update(data).digest('hex');
+  const hashV1 = crypto.createHash('sha256').update(data).digest('hex');
+  const hashV2 = ':' + crypto.createHash('sha256').update(data).digest('base64') + ':';
   const compressed = await fs.readFile(`data/shop.dict.br`);
-  return { hash: hash, compressed: compressed };
+  return { hashV1: hashV1, hashV2: hashV2, compressed: compressed };
 }
 
 async function loadItemData(id) {
@@ -59,16 +60,20 @@ ITEMS.forEach((id) => {
     reply.header('content-type', 'text/html; charset=utf-8');
     reply.header('cache-control', 'public, max-age=100');
     reply.header('vary', 'sec-available-dictionary');
-    const dictHash = request.headers['sec-available-dictionary'];
+    const v1Matched = request.headers['sec-available-dictionary'] == dictionary.hashV1;
+    const v2Matched = request.headers['available-dictionary'] == dictionary.hashV2;
     const acceptEncodings = request.headers['accept-encoding'].split(',');
     const sbrSupported = acceptEncodings.some(x => x.trim()=='sbr');
     const szstSupported = acceptEncodings.some(x => x.trim()=='zstd-d');
-    if (dictHash == dictionary.hash && (sbrSupported || szstSupported)) {
+    if ((v1Matched || v2Matched) && (sbrSupported || szstSupported)) {
+      if (v2Matched) {
+        reply.header('content-dictionary', dictionary.hashV2);
+      }
       if (szstSupported) {
         reply.header('content-encoding', 'zstd-d');
         reply.send(Buffer.from(items[id].szst));
       } else {
-        reply.header('content-encoding', 'sbr');
+        reply.header('content-encoding', v2Matched ? 'br-d' : 'sbr');
         reply.send(Buffer.from(items[id].sbr));
       }
     } else {
